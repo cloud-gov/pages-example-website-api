2c5b839dca3bf85f05437eadb4801dd5db45962c
Ephraim-G <ephraim.gsa.gov>
2023-12-05 13:31:28 -0500

    init repo example app

---
+++ app.py
@@ -0,0 +1,42 @@
+import psycopg2
+import os
+from flask import Flask, jsonify
+from cfenv import AppEnv
+from psycopg2.extras import RealDictCursor
+from flask_cors import CORS
+
+
+app = Flask(__name__)
+app_env = AppEnv()
+CORS(app, origins=['https://federalist-c8f0d18e-b710-44dc-9412-4b4f26efb0a3.sites.pages.cloud.gov'], headers=['Content-Type'], methods=['GET'])
+
+port = int(os.getenv('PORT', 8080)) 
+    
+aws_rds = app_env.get_service(name='hurricane')
+    
+connection = psycopg2.connect(
+    host=aws_rds.credentials.get('host'),
+    user=aws_rds.credentials.get('username'),
+    password=aws_rds.credentials.get('password'),
+    database=aws_rds.credentials.get('name'),
+    port=aws_rds.credentials.get('port'),
+)
+@app.route('/', methods=['GET'])
+def hello():
+    return 'There is a table right behind this door!'
+
+@app.route('/get_table', methods=['GET'])
+def get_table():
+    cursor = connection.cursor(cursor_factory=RealDictCursor)
+    
+    query = 'SELECT * FROM fdic_banks LIMIT 15'
+    cursor.execute(query)
+    
+    rows = cursor.fetchall()
+    cursor.close()
+    
+    return jsonify(rows)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=port)
+
---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='cypyapi',
+    version='1.0.0',
+    description='cloud.gov API',
+    author='Ephraim',
+    packages=['Santas bag'],
+)
