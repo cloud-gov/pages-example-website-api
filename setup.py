6d5d8307748686c99cc642a4f49c389afd1cc454
Ephraim-G <ephraim.gsa.gov>
2023-12-05 16:02:06 -0500

    more formatting

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     install_requires=[
         'aiohttp==3.8.4',
         'aiosignal==1.3.1',
-        'apispec'==6.0.2
+        'apispec'==6.0.2,
         'async-timeout'==4.0.2,
         'attrs'==22.2.0,
         'awscli-local'==0.20,
