be96cdaddc6d9866c8102322c7eb5c7e249c9609
Ephraim-G <ephraim.gross@gsa.gov>
2024-03-06 12:36:03 -0500

    update deployment env and requirements.txt

--- app.py
+++ app.py
@@ -5,38 +5,41 @@ from cfenv import AppEnv
 from psycopg2.extras import RealDictCursor
 from flask_cors import CORS
 
+origin = os.getenv("ORIGIN")
+port = int(os.getenv("PORT", 8080))
 
 app = Flask(__name__)
+CORS(app, origins=origin)
+
 app_env = AppEnv()
-CORS(app, origins=['https://federalist-31c21015-f923-4733-95df-2e979da3e393.sites.pages.cloud.gov/site/cloud-gov/pages-example-api-website/'], headers=['Content-Type'], methods=['GET'])
+aws_rds = app_env.get_service(name="example-website-api-databse")
 
-port = int(os.getenv('PORT', 8080)) 
-    
-aws_rds = app_env.get_service(name='hurricane')
-    
 connection = psycopg2.connect(
-    host=aws_rds.credentials.get('host'),
-    user=aws_rds.credentials.get('username'),
-    password=aws_rds.credentials.get('password'),
-    database=aws_rds.credentials.get('name'),
-    port=aws_rds.credentials.get('port'),
+    host=aws_rds.credentials.get("host"),
+    user=aws_rds.credentials.get("username"),
+    password=aws_rds.credentials.get("password"),
+    database=aws_rds.credentials.get("name"),
+    port=aws_rds.credentials.get("port"),
 )
-@app.route('/', methods=['GET'])
-def hello():
-    return 'There is a table right behind this door!'
 
-@app.route('/get_table', methods=['GET'])
+
+@app.route("/", methods=["GET"])
+def hello():
+    return "There is a table right behind this door!"
+
+
+@app.route("/get_table", methods=["GET"])
 def get_table():
     cursor = connection.cursor(cursor_factory=RealDictCursor)
-    
-    query = 'SELECT * FROM fdic_banks LIMIT 15'
+
+    query = "SELECT * FROM fdic_banks LIMIT 15"
     cursor.execute(query)
-    
+
     rows = cursor.fetchall()
     cursor.close()
-    
+
     return jsonify(rows)
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=port)
 
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=port)
